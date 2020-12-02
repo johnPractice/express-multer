ea1e6ec88dc89d2bfe2f558215ac38e0b2d7a893
johnpractice <rezamosavian1010@gmail.com>
2020-12-02 14:22:00 -0500

    beati code

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
-app.listen(port , () => {
-    console.log('conected');
+app.listen(port, () => {
+    console.log(`serever run at ${port}`);
 });
